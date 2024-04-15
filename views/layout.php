b1e9943fa6f480b0f7e4ce488c4dd1a34372d81e
cdhepgdo <166970212+cdhepgdo@users.noreply.github.com>
2024-04-15 11:49:06 -0400

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -61,10 +61,10 @@ if(!isset($inicio)){
     <footer class="footer seccion">
         <div class="contenedor contenedor-footer">
             <nav class="navegacion">
-                <a href="./nosotros">Nosotros</a>
-                <a href="./propiedades">Anuncios</a>
-                <a href="./blog">Blog</a>
-                <a href="./contacto">Contacto</a>
+                <a href="/nosotros">Nosotros</a>
+                <a href="/propiedades">Anuncios</a>
+                <a href="/blog">Blog</a>
+                <a href="/contacto">Contacto</a>
             </nav>
         </div>
 
